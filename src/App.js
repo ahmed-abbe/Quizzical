c42403af03cbb1490ff0cea43bcf9be336d23c77
ahmed-abbe <ahmedmohamed3615@gmail.com>
2023-12-06 10:43:25 +0200

    Quiz App V1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Quiz from "./components/Quiz";
+import blueBlob from "./images/blobs-blue.png";
+import YellowBlob from "./images/blobs-yellow.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <section className="quiz">
+            <img src={YellowBlob} alt="yellow blob" />
+            <div className="container">
+                <Quiz />
+            </div>
+            <img src={blueBlob} alt="blue blob" />
+        </section>
+    );
+};
 
 export default App;
